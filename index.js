a61323924ea6f85a74fd5809ba4aca28cd88de98
DayanaraReynaa <dayanara.reynaa@gmail.com>
2023-12-25 22:57:45 -0800

    Cleaned up some JS code, fixed clear button, added max canvas size

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
+// Functions
+
+function paintGrid () {
+    const gridSquares = document.querySelectorAll('.grid-square');
+    gridSquares.forEach(gridSquare => {
+        gridSquare.addEventListener('mouseover', () => {
+            gridSquare.style.backgroundColor = color;
+        });
+    });
+}
+
+// ----------------------------------------------------
+
+function createCanvas() {
+    for (let i = 0; i < gridSize; i++) {
+        const newRow = document.createElement('div');
+        newRow.classList.add('row');
+
+        for (let j = 0; j < gridSize; j++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('grid-square');
+            newDiv.style.width = squareSize + 'px';
+            newDiv.style.height = squareSize + 'px';
+            newRow.appendChild(newDiv);
+        }
+
+        sketchContainer.appendChild(newRow);
+    }
+    paintGrid();
+}
+
+
+// ----------------------------------------------------
+
+
+// ----------------------------------------------------
+// Variables I
 const sketchContainer = document.querySelector('.sketch-container');
 let color = 'black';
 let gridSize = 16;
+let squareSize = 35;
 
-for (let i = 0; i < gridSize; i++) {
-    const newRow = document.createElement('div');
-    newRow.classList.add('row');
-    
-    for (let j = 0; j < gridSize; j++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('grid-square');
-        newRow.appendChild(newDiv);
-    }
+// Initial Canvas
+createCanvas();
 
-    sketchContainer.appendChild(newRow);
-}
-
-// Hover Squares
-
-const gridSquares = document.querySelectorAll('.grid-square');
-
-gridSquares.forEach(gridSquare => {
-    gridSquare.addEventListener('mouseover', () => {
-        gridSquare.style.backgroundColor = color;
-    });
-});
-
+// ----------------------------------------------------
 // Button Event Listeners
 const drawBtn = document.getElementById('draw-btn');
 const eraseBtn = document.getElementById('eraser-btn');
 const clearBtn = document.getElementById('clear-btn');
-
+const enterBtn = document.getElementById('grid-btn');
+// ----------------------------------------------------
 
 drawBtn.addEventListener('click', function () {
     color = 'black';
@@ -39,61 +59,24 @@ eraseBtn.addEventListener('click', function () {
     color = '#f7f0f5';
 });
 
-
 clearBtn.addEventListener('click', function () {
+    const gridSquares = document.querySelectorAll('.grid-square');
     gridSquares.forEach(gridSquare => {
-            gridSquare.style.backgroundColor = '#f7f0f5';
+        gridSquare.style.backgroundColor = '#f7f0f5';
     });
-    
 });
-
-
-
-
-
-
-
-
-// Grid Size Changes
-const enterBtn = document.getElementById('grid-btn');
 
 enterBtn.addEventListener('click', function () {
     // Get the new gridSize from the input
     gridSize = parseInt(document.querySelector('.grid-input').value);
 
     // Check if gridSize is a valid number
-    if (!isNaN(gridSize)) {
-        // Calculate the size of each grid square based on the fixed canvas size
-        const squareSize = 560 / gridSize;
-
+    if (isNaN(gridSize) || gridSize > 100) {
+       gridSize = 16;
+    }
+        squareSize = 560 / gridSize;
         // Clear the existing grid
         sketchContainer.innerHTML = '';
-
         // Create a new grid
-        for (let i = 0; i < gridSize; i++) {
-            const newRow = document.createElement('div');
-            newRow.classList.add('row');
-
-            for (let j = 0; j < gridSize; j++) {
-                const newDiv = document.createElement('div');
-                newDiv.classList.add('grid-square');
-
-                // Set the size of each grid square
-                newDiv.style.width = squareSize + 'px';
-                newDiv.style.height = squareSize + 'px';
-
-                newRow.appendChild(newDiv);
-            }
-
-            sketchContainer.appendChild(newRow);
-        }
-
-        // Update the hover event for the new grid squares
-        const newGridSquares = document.querySelectorAll('.grid-square');
-        newGridSquares.forEach(newGridSquare => {
-            newGridSquare.addEventListener('mouseover', () => {
-                newGridSquare.style.backgroundColor = color;
-            });
-        });
-    }
+        createCanvas();
 });
